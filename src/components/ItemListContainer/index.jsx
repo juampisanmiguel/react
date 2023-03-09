596292b66f25b472806e59a2eaec4261f41cead0
Juan Pablo San Miguel <juampisanmiguel@hotmail.com>
2023-03-09 18:51:56 -0300

    primer pre entrega

---
+++ src/components/ItemListContainer/index.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import Title from '../Title'
+
+
+export const ItemListContainer = () => {
+    return (
+        <Title greeting= 'No hay productos disponibles' />
+    );
+}
+
+export default ItemListContainer;
--- src/components/NavBar/index.jsx
+++ src/components/NavBar/index.jsx
@@ -1,29 +1,36 @@
 import React from 'react';
+import CartWidget from '../CartWidget'
+
 
 export const NavBar = () => {
     return (
-        <nav class="navbar navbar-expand-lg navbar-light bg-light">
-  <div class="container-fluid">
-    <a class="navbar-brand" href="#">Navbar</a>
-    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
+        <nav className="navbar navbar-expand-lg navbar-light bg-light">
+  <div className="container-fluid">
+    <a className="navbar-brand" href="#">SM vintage</a>
+    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+      <span className="navbar-toggler-icon"></span>
     </button>
-    <div class="collapse navbar-collapse" id="navbarNav">
-      <ul class="navbar-nav">
-        <li class="nav-item">
-          <a class="nav-link active" aria-current="page" href="#">Inicio</a>
+    <div className="collapse navbar-collapse" id="navbarNav">
+      <ul className="navbar-nav">
+        <li className="nav-item">
+          <a className="nav-link active" aria-current="page" href="#">Inicio</a>
         </li>
-        <li class="nav-item">
-          <a class="nav-link" href="#">Remeras</a>
+        <li className="nav-item">
+          <a className="nav-link" href="#">Remeras</a>
         </li>
-        <li class="nav-item">
-          <a class="nav-link" href="#">Pantalones</a>
+        <li className="nav-item">
+          <a className="nav-link" href="#">Pantalones</a>
         </li>
-        <li class="nav-item">
-          <a class="nav-link" href="#">Buzos</a>
+        <li className="nav-item">
+          <a className="nav-link" href="#">Buzos</a>
         </li>
-        <li class="nav-item">
-          <a class="nav-link" href="#">Accesorios</a>
+        <li className="nav-item">
+          <a className="nav-link" href="#">Accesorios</a>
+        </li>
+        <li className="nav-item">
+          <a className="nav-link" href="#">
+            <CartWidget/>
+            </a>
         </li>
       </ul>
     </div>
